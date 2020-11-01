97ac918b82509d2c21ac42c89d1ac9bfef6ce806
Chestnut-Home <974920378@qq.com>
2020-11-01 16:40:39 +0800

    [CodeSnippet][1/1]{去除无用的废代码}
    适用范围：{>=Q}
    准入id：{NA}
    分析：{去除无用的废代码}
    方案：{去除无用的废代码}
    风险及影响[快/稳/省/功能/安全隐私]：{NA}
    测试建议：{NA}
    跨组依赖：{NA}

--- app/src/main/java/x/chestnut/code/snippet/other/asyncTask/AsyncTaskExampleFragment.java
+++ app/src/main/java/x/chestnut/code/snippet/other/asyncTask/AsyncTaskExampleFragment.java
@@ -1,15 +1,8 @@
 package x.chestnut.code.snippet.other.asyncTask;
 
-import android.arch.lifecycle.Observer;
 import android.os.AsyncTask;
-import android.support.annotation.Nullable;
 import android.view.View;
 
-import java.util.List;
-
-import androidx.work.OneTimeWorkRequest;
-import androidx.work.WorkInfo;
-import androidx.work.WorkManager;
 import x.chestnut.code.snippet.base.ScrollBaseFragment;
 
 /**
@@ -44,26 +37,10 @@ public class AsyncTaskExampleFragment extends ScrollBaseFragment {
             MyAsyncTask myAsyncTask = new MyAsyncTask();
             myAsyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,"strings");
         });
-
-        String WORK_TAG = "WLAN_DETECT";
-        OneTimeWorkRequest request = new OneTimeWorkRequest.Builder(WlanDetectWork.class)
-                .addTag(WORK_TAG)
-                .build();
-        WorkManager.getInstance().enqueue(request);
-        WorkManager.getInstance().cancelAllWorkByTag(WORK_TAG);
-        WorkManager.getInstance().getWorkInfosByTagLiveData(WORK_TAG)
-                .observe(this, new Observer<List<WorkInfo>>() {
-                    @Override
-                    public void onChanged(@Nullable List<WorkInfo> workInfos) {
-
-                    }
-                });
     }
 
     @Override
     protected void onViewResume() {
         setTitle("AsyncTask");
     }
-
-
 }
