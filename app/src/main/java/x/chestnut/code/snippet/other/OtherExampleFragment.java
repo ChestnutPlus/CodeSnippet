14f80a76134c3794ea1b9e59d0bd6b59e8bfa411
Chestnut-Home <974920378@qq.com>
2020-11-14 00:08:23 +0800

    [CodeSnippet][1/1]{增加 Gradle 多渠道示例}
    适用范围：{>=Q}
    准入id：{NA}
    分析：{xxx}
    方案：{xxx}
    风险及影响[快/稳/省/功能/安全隐私]：{NA}
    测试建议：{NA}
    跨组依赖：{NA}

--- app/src/main/java/x/chestnut/code/snippet/other/OtherExampleFragment.java
+++ app/src/main/java/x/chestnut/code/snippet/other/OtherExampleFragment.java
@@ -1,7 +1,11 @@
 package x.chestnut.code.snippet.other;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
 import android.view.View;
 
+import x.chestnut.code.snippet.BuildConfig;
 import x.chestnut.code.snippet.base.ScrollBaseFragment;
 import x.chestnut.code.snippet.other.asyncTask.AsyncTaskExampleFragment;
 
@@ -18,6 +22,8 @@ import x.chestnut.code.snippet.other.asyncTask.AsyncTaskExampleFragment;
  */
 public class OtherExampleFragment extends ScrollBaseFragment {
 
+    private static final String TAG = OtherExampleFragment.class.getName();
+
     public OtherExampleFragment() {}
 
     public static OtherExampleFragment newInstance() {
@@ -27,6 +33,18 @@ public class OtherExampleFragment extends ScrollBaseFragment {
     @Override
     protected void onLazyViewCreate(View rootView) {
         addView("AsyncTask", view -> startFragment(AsyncTaskExampleFragment.newInstance()));
+        addView("GradleTest", view -> {
+            Log.e(TAG, "gradleInfo, name: " + BuildConfig.NAME);
+            Log.e(TAG, "gradleInfo, is_debug: " + BuildConfig.IS_DEBUG);
+            try {
+                ApplicationInfo applicationInfo = getContext().getPackageManager().getApplicationInfo(
+                        BuildConfig.APPLICATION_ID, PackageManager.GET_META_DATA);
+                String result = applicationInfo.metaData.getString("NAME", "not found");
+                Log.e(TAG, "result: " + result);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
